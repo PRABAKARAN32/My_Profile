2ff43c608b6bcbb909cdf6c14ac820b72727eea0
Prabakaran J <prabakaranabcabc@gmail.com>
2025-04-28 20:33:05 +0530

    Final Touch

--- React_folder/vite.config.js
+++ React_folder/vite.config.js
@@ -6,6 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0',
-    port: 5173
+    port: 80,
+    // strictPort: false // Allow dynamic port allocation if the desired port is occupied
   }
 })
