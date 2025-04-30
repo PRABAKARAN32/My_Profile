d3d479761a957697e956c08e77d9fa9c4d52d5d8
Prabakaran J <prabakaranabcabc@gmail.com>
2025-04-30 18:19:06 +0530

    Issue in the Skill section has been fixed

--- React_folder/src/components/Skills/Skills.jsx
+++ React_folder/src/components/Skills/Skills.jsx
@@ -6,32 +6,30 @@ import Tilt from "react-parallax-tilt";
 const Skills = () => (
   <section
     id="skills"
-    className="py-24 pb-24 px-[12vw] md:px-[7vw] lg:px-[20vw] font-sans bg-skills-gradient clip-path-custom"
+    className="py-24 px-6 sm:px-[12vw] md:px-[7vw] lg:px-[10vw] font-sans bg-skills-gradient clip-path-custom"
   >
     {/* Section Title */}
-    <div className="text-center mb-8">
+    <div className="text-center mb-12">
       <h2 className="text-3xl sm:text-4xl font-bold text-white">SKILLS</h2>
       <div className="w-24 h-1 bg-[#8245ec] mx-auto mt-2"></div>
       <p className="text-gray-400 mt-4 text-lg font-semibold">
-      A collection of my technical skills and expertise honed through various projects and experiences
+        A collection of my technical skills and expertise honed through various projects and experiences
       </p>
     </div>
 
     {/* Skill Categories */}
-    <div className="flex flex-wrap gap-1 lg:gap-5 py-10 justify-between">
+    <div className="flex flex-col md:flex-col lg:flex-row lg:flex-wrap gap-6 justify-between items-center">
       {SkillsInfo.map((category) => (
         <div
           key={category.title}
-          className="bg-gray-900 backdrop-blur-md px-6 sm:px-10 py-8 sm:py-6 mb-10 w-full sm:w-[48%] rounded-2xl border border-white 
+          className="bg-gray-900 backdrop-blur-md px-6 sm:px-10 py-8 sm:py-6 w-full lg:w-[45%] rounded-2xl border border-white 
           shadow-[0_0_20px_1px_rgba(130,69,236,0.3)]"
         >
           <h3 className="text-2xl sm:text-3xl font-semibold text-gray-400 mb-4 text-center">
             {category.title}
           </h3>
 
-          {/* Skill Items - 3 per row on larger screens */}
           <Tilt
-            key={category.title}
             tiltMaxAngleX={20}
             tiltMaxAngleY={20}
             perspective={1000}
@@ -39,11 +37,11 @@ const Skills = () => (
             transitionSpeed={1000}
             gyroscope={true}
           >
-            <div className="grid grid-cols-2 sm:grid-cols-3 gap-3 w-full">
+            <div className="grid grid-cols-2 sm:grid-cols-3 gap-4">
               {category.skills.map((skill) => (
                 <div
                   key={skill.name}
-                  className="flex items-center justify-center space-x-2 bg-transparent border-2 border-gray-700 rounded-3xl py-2 px-2 sm:py-2 sm:px-2 text-center"
+                  className="flex items-center justify-center gap-2 bg-transparent border-2 border-gray-700 rounded-3xl py-2 px-3 text-center"
                 >
                   <img
                     src={skill.logo}
