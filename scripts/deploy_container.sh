3eedb60aa573d11145275f54851a74281054a801
Prabakaran32 <prabakaranabcabc@gmail.com>
2026-02-04 14:22:54 +0530

    CICD Set up is done for now to run in lab enveronment

--- scripts/deploy_container.sh
+++ scripts/deploy_container.sh
@@ -1,29 +1,18 @@
 #!/bin/bash
+set -e
 
-# Inputs
-DEPLOY_IMAGE="$1"
+echo "Starting deployment..."
 
-# Extract the image name without the tag (before the colon)
-IMAGE_NAME=$(echo "$DEPLOY_IMAGE" | sed 's/:.*//')
+cd React_folder
 
-# Extract the tag (after the colon) for pulling the image
-IMAGE_TAG=$(echo "$DEPLOY_IMAGE" | sed 's/.*://')
+npm install
+npm run build
 
-# Step 1: Find and stop old containers
-old_containers=$(docker ps --format "{{.ID}} {{.Image}}" | awk -F '[: ]' '$2=="prabakaran32/portfolio" {print $1}')
+cd ..
 
-if [ ! -z "$old_containers" ]; then
-  echo "Stopping and removing old containers..."
-  docker stop $old_containers
-  docker rm -f $old_containers
-else
-  echo "No old containers found. Continuing deployment..."
-fi
+rm -rf ~/htdocs/Myprofile
+mkdir -p ~/htdocs/Myprofile
 
-# Step 2: Pull the new image with the tag
-echo "Pulling new image: $DEPLOY_IMAGE"
-docker pull "$DEPLOY_IMAGE"
+mv React_folder/dist ~/htdocs/Myprofile/files
 
-# Step 3: Run the new container
-echo "Running new container from: $DEPLOY_IMAGE"
-docker run -d -p 80:3000 "$DEPLOY_IMAGE"
+echo "Deployment completed"
