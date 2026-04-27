a9d9765cc2a1ee6972181fd00872168115dc8d60
Prabakaran32 <prabakaranabcabc@gmail.com>
2026-04-27 21:45:52 +0530

    The Resume link is updated

--- React_folder/src/components/About/About.jsx
+++ React_folder/src/components/About/About.jsx
@@ -48,7 +48,7 @@ const About = () => {
           </p>
           {/* Resume Button */}
           <a
-            href="https://drive.google.com/file/d/1Mrm-MkKBjb3tF8oE6_urO4oBdzuX5xst/view?usp=drive_link"
+            href="https://drive.google.com/file/d/1nwX1qKYbWuFtbtMfDN6JZV_0iWHkzbMg/view?usp=drive_link?usp=drive_link"
             target="_blank"
             rel="noopener noreferrer"
             className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
