a0c10a35e1f5c26fa642bbbab2bda3f6baaa1b81
Prabakaran32 <prabakaranabcabc@gmail.com>
2026-07-20 21:52:04 +0530

    Resume Updated.

--- React_folder/src/components/About/About.jsx
+++ React_folder/src/components/About/About.jsx
@@ -51,7 +51,7 @@ const About = () => {
           </p>
           {/* Resume Button */}
           <a
-            href="https://drive.google.com/file/d/1nwX1qKYbWuFtbtMfDN6JZV_0iWHkzbMg/view?usp=drive_link"
+            href="https://drive.google.com/file/d/174Xlo5C5e-EtvJvvWCjajnEaA1AzZiSI/view?usp=sharing"
             target="_blank"
             rel="noopener noreferrer"
             className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
